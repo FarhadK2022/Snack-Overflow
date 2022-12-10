b340d4491e23bc9cdfe01216f4b2286368956821
ricalope <mrrobot421@gmail.com>
2022-12-10 13:47:40 -0700

    added get all questions route

--- app/api/questions_routes.py
+++ app/api/questions_routes.py
@@ -1,2 +1,9 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import Question, db
+
+questions_routes = Blueprint("questions", __name__)
+
+@questions_routes.route("/")
+def get_all_questions():
+    questions = Question.query.all()
+    return { question.id: question.to_dict() for question in questions }
