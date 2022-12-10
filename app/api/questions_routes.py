d62ff367fa519132c9933de5df1f2cb11d1354a6
Farhad Koushan <farhadkoushan@gmail.com>
2022-12-10 14:54:21 -0800

    post route

--- app/api/questions_routes.py
+++ app/api/questions_routes.py
@@ -1,8 +1,19 @@
 from flask import Blueprint, jsonify, session, request
-flask_login import current_user
+from flask_login import current_user, login_required
 from app.models import Question, db
+from app.forms import QuestionForm
 
 questions_routes = Blueprint("questions", __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 @questions_routes.route("/")
 def get_all_questions():
@@ -16,23 +27,26 @@ def get_one_question(id):
 
 
 @questions_routes.route("/", methods=["POST"])
+@login_required
 def add_question():
+
   form = QuestionForm()
+
+  form['csrf_token'].data = request.cookies['csrf_token']
 
   if form.validate_on_submit():
 
-   question = Question(
-    user_id = current_user.id,
-    title = form.data['title'],
-    question = form.data['question'],
-    tried_expected = form.data['tried_expected'],
-    tags = form.data['tags']
-  )
+    question = Question(
+      user_id = current_user.id,
+      title = form.data['title'],
+      question = form.data['question'],
+      tried_expected = form.data['tried_expected'],
+      tags = form.data['tags']
+    )
 
-  db.session.add(question)
-  db.session.commit()
+    db.session.add(question)
+    db.session.commit()
+    return question.to_dict()
 
-  if form.errors:
-    return form.errors
 
-  return
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
