9f69f875f0474164bae5e577ac2e0927511946be
zswanson92 <zackta92@gmail.com>
2022-12-09 18:18:28 -0600

    Added add_prefix_for_prod to foreign keys

--- app/models/downvote.py
+++ app/models/downvote.py
@@ -10,8 +10,8 @@ class Downvote(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    answer_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('answers.id')), nullable=False)
 
     downvote_user_relationship = db.relationship("User", overlaps="downvote_relationship")
     downvote_answer_relationship = db.relationship("Answer", back_populates="answer_downvote_relationship")
--- app/models/question.py
+++ app/models/question.py
@@ -10,7 +10,7 @@ class Question(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     title = db.Column(db.String(150), nullable=False)
     question = db.Column(db.Text, nullable=False)
     tried_expected = db.Column(db.Text, nullable=False)
