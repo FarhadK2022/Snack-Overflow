4edda148eab220a3f761cf09d6b07ac8de342747
Farhad Koushan <farhadkoushan@gmail.com>
2022-12-12 18:56:02 -0800

    updated the join tables per instructions in mod 6 resources

--- app/models/question.py
+++ app/models/question.py
@@ -5,8 +5,8 @@ import datetime
 likes = db.Table(
     "likes",
     db.Model.metadata,
-    db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id"))),
-    db.Column("question_id", db.Integer, db.ForeignKey(add_prefix_for_prod("questions.id")))
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("question_id", db.Integer, db.ForeignKey("questions.id"))
 )
 
 if environment == "production":
