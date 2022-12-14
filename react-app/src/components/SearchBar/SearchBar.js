48127d48b49b3b467879f1581f5f145530755bbb
Farhad Koushan <farhadkoushan@gmail.com>
2022-12-14 01:06:52 -0800

    ironed out all of the search bar bugs

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,51 +1,54 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import {  Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { getResultsThunk } from "../../store/search";
-import './SearchBar.css'
+import "./SearchBar.css";
 
 const SearchBar = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [searchInput, setSearchInput] = useState("");
-  const [filter, setFilter] = useState("title");
+  const [filter, setFilter] = useState("");
+  const history = useHistory();
 
-  const  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     const searchResults = await dispatch(getResultsThunk(filter, searchInput));
-    if(searchResults){
-      setFilter("title")
-      return setSearchInput("")
+
+    if (searchResults) {
+      return history.push(`/search/?question=${searchInput}&filter=${filter}`);
     }
   };
 
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Search..."
-        defaultValue={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        required
-        className="search-bar"
-      />
-      <label>
-        Filters
-      <select
-          className="queryParams"
-          type="text"
-          defaultValue={filter}
-          onChange={(e) => setFilter(e.target.value)}
-        >
-          <option value={'title'}>By Title</option>
-          <option value={'body'}>By Content</option>
-          <option value={'tags'}>By Tags</option>
-        </select>
-      </label>
-       <button type="submit">Search</button>
-    </form>
-    <Redirect to={`/search/?question=${searchInput}&filter=${filter}`}/>
-  </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="🔍Search..."
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          required
+          className="search-bar"
+        />
+        <label>
+          Filters
+          <select
+            className="queryParams"
+            type="text"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            required
+          >
+            <option placeholder=""></option>
+            <option value={"title"}>By Title</option>
+            <option value={"body"}>By Content</option>
+            <option value={"tags"}>By Tags</option>
+          </select>
+        </label>
+        <button type="submit">🔍</button>
+      </form>
+    </div>
   );
 };
 
--- react-app/src/components/SearchBar/SearchResultsPage.js
+++ react-app/src/components/SearchBar/SearchResultsPage.js
@@ -1,43 +1,45 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom'
-import { getResultsThunk } from '../../store/search';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const SearchResults = () => {
-    const dispatch = useDispatch()
+  const searchObj = useSelector((state) => {
+    return state;
+  });
 
-    const searchObj = useSelector(state => {
-        return state
-    })
+  const search = Object.values(searchObj.searchReducer.allResults);
 
-    const search = Object.values(searchObj.searchReducer.allResults)
-
-
-    useEffect(() => {
-        dispatch(getResultsThunk())
-    }, [dispatch])
-
-
-
+  if (search.length === 0) {
     return (
-        <div className='search-results-div'>
-            <h2>{search.map((obj) => {
-                return(
-                    <div key={obj.id}>
-                        <Link style={{textDecoration: 'none'}} to={`/questions/${obj.id}`}>
-                        <p>Title: {obj.title}</p>
-                        {/*
+      <div>
+        <h1>⚠️🍽⚠️</h1>
+        <h2>🍩No Results Found🍩</h2>
+        <h3>☞Please Check Your Search a🔍nd Try Again☜</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="search-results-div">
+        <h2>
+          {search.map((obj) => {
+            return (
+              <div key={obj.id}>
+                <Link
+                  style={{ textDecoration: "none" }}
+                  to={`/questions/${obj.id}`}
+                >
+                  <p>Title: {obj.title}</p>
+                  {/*
 
-                         */}
-                        </Link>
-                    </div>
-                )
-            })}
-            </h2>
-        </div>
-    )
-}
+                           */}
+                </Link>
+              </div>
+            );
+          })}
+        </h2>
+      </div>
+    );
+  }
+};
 
-
-export default SearchResults
+export default SearchResults;
